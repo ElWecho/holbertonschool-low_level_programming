4a78683874c4839d00d5e97b2621cc221042da7a
ElWecho <guartecheagus@gmail.com>
2021-09-30 10:21:29 -0700

    Q5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
+/**
+ *  * main - The main function serves for the execution of the program.
+ *   * Return: 0
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
