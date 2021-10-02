1536c28f04a50663be36de641a9a74d2018821be
ElWecho <guartecheagus@gmail.com>
2021-10-01 19:51:34 -0700

    Q6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,13 +3,15 @@
 /**
  * main - print all single numbers
  *
- * Return 0
+ * Return: 0
  */
 int main(void)
 {
 	int digit;
+
 	for (digit = 0; digit < 10; digit++)
 	putchar(digit + '0');
 	putchar('\n');
+
 	return (0);
 }
