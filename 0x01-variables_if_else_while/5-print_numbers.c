2d19682aebb977cedcb6601a1c86296c4d69635d
ElWecho <guartecheagus@gmail.com>
2021-10-01 19:33:28 -0700

    Q5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print all single numbers
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+	printf("%i", digit);
+	putchar('\n');
+
+	return (0);
+}
