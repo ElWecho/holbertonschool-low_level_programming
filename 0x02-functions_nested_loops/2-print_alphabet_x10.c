77e5770f05b096b44aeb3c10d99f2588ff4522aa
ElWecho <guartecheagus@gmail.com>
2021-10-06 09:00:18 -0700

    Q2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - Prints the alphabet 10 times.
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int repeat, message;
+
+		for (repeat = 0; repeat <= 9; repeat++)
+	
+	{
+		for (message = 97; message <= 122; message++)
+		{
+			_putchar(message);
+		}
+		_putchar('\n');
+	}
+}
