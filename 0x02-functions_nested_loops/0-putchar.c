a4329dca8d080e83555e353fdd0a0945d38faa99
ElWecho <guartecheagus@gmail.com>
2021-10-06 07:45:00 -0700

    Q0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Write a program that prints _putchar, followed by a new line.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	char message[] = "_putchar";
+	for (char = 0; char = message; char++)
+		putchar('\n');
+
+	return (0)
+}
