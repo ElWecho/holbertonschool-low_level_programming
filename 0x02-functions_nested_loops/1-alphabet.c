8cb388ba7511123e51aeea3de611cf6a379a1c57
ElWecho <guartecheagus@gmail.com>
2021-10-06 08:27:31 -0700

    Q1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Print the alphabet in lowercase.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+		_putchar('\n');
+
+	return (0);
+}
