9e8a9ed9407e16eadc86f818b640ad6026b22b86
ElWecho <guartecheagus@gmail.com>
2021-11-03 18:04:14 -0700

    Q0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints name
+ * @name: pointer to a name
+ * @f: pointer to function
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (f != 0)
+		f(name);
+}
