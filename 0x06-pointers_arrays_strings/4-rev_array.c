192fb5fd3dd7af7a5ad307dc6849c79cbb4c290c
ElWecho <guartecheagus@gmail.com>
2021-11-03 19:10:57 -0700

    Q4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse array
+ * Description: of integers
+ * Return: reversed array
+ * @a: array
+ * @n: number of elements
+ */
+
+void reverse_array(int *a, int n)
+{
+int swap, i, j = n - 1;
+
+for (i = 0; i < n / 2; i++)
+{
+	swap = a[i];
+	a[i] = a[j];
+	a[j--] = swap;
+}
+}
