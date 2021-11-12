c043fe4e95bee6d129b326a28ec2e4be38dc3c0f
ElWecho <guartecheagus@gmail.com>
2021-11-11 16:45:55 -0800

    Q100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * cleanupfunc - function that prints before main
+ * Return: void
+ */
+
+void cleanupfunc(void)
+{
+	char *str1 = "You're beat! and yet, you must allow,\n";
+	char *str2 = "I bore my house upon my back!\n";
+
+	printf("%s%s", str1, str2);
+}
